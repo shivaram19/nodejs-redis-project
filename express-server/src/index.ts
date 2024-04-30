5f7e0e0f1b6543f40a5e7868e966911c0729e8f3
ShivaramMorigala <shivaramiitmb085@gmail.com>
2024-05-01 00:40:13 +0530

    hakuna

---
+++ express-server/src/index.ts
@@ -0,0 +1,41 @@
+import express from "express"
+import { createClient } from "redis"
+
+const app = express()
+app.use(express.json())
+
+const client = createClient();
+client.on("error", function(err){
+	console.log("redis cleint error", err)
+})
+
+app.post("/submit",async function(req, res){
+	const { problemId, userId, code , language } = req.body
+	// push this to database
+	try {
+		await client.lPush("submissions", JSON.stringify({ problemId, userId, code , language }))
+			res.json({
+			message:"submission received"
+		})
+	} catch (error) {
+		console.log("reddis -error", error)
+		res.json({
+			message:"submission failed"
+		})
+	} 
+
+})
+		
+async function startServer(){
+	try {
+		await client.connect()
+		console.log("connected to redis ")
+		app.listen(8070, function(){
+			console.log("server is connected")
+		})
+	} catch (error) {
+		console.log("redis connection error ", error)
+	}
+}
+
+startServer()
